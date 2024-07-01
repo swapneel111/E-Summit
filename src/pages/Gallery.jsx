ceec5eab59df1ce15b3f976b02d43e10a88c2df8
Swapneel Chaudhuri <113018040+swapneel111@users.noreply.github.com>
2024-07-01 13:44:36 +0530

    changed

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,15 +1,6 @@
 import React from 'react'
 import bridge from '../assets/bridge.png'
 import './Gallery.css'
-import g2 from '../assets/gallery/1666612288642.jpeg'
-import g3 from '../assets/gallery/1666612290097.jpeg'
-import g4 from '../assets/gallery/1666612290243.jpeg'
-import g5 from '../assets/gallery/1666612290295.jpeg'
-import g6 from '../assets/gallery/1666632949384.jpeg'
-import g7 from '../assets/gallery/1666632949980.jpeg'
-import g8 from '../assets/gallery/1666633727766.jpeg'
-import g9 from '../assets/gallery/1666633728084.jpeg'
-import g10 from '../assets/gallery/1666633728162.jpeg'
 import g11 from '../assets/logos/web dev.png'
 
 export default function Gallery() {
