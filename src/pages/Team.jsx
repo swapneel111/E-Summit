6544ef66878e39b714854a1c744f7e1eef6d1786
Swapneel Chaudhuri <113018040+swapneel111@users.noreply.github.com>
2024-07-03 23:44:44 +0530

    teams

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -18,7 +18,7 @@ export default function Team() {
     <div class="property-description">
       <h5>Swapneel Chaudhuri</h5>
       <p>Event Head</p>
-      <a href=""><img class="linklogo" src={linklogo} alt=""/></a>
+      <a href="https://www.linkedin.com/in/swapneel-chaudhuri-168ba226b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
     </div>
     <a href="/">
     </a>
@@ -35,7 +35,7 @@ export default function Team() {
     <div class="property-description">
       <h5> Sagnik Sinha</h5>
       <p>Event Head</p>
-      <a href=""><img class="linklogo" src={linklogo} alt=""/></a>
+      <a href="https://www.linkedin.com/in/sagnik-sinha?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
     </div>
     <a href="/">
     </a>
@@ -52,7 +52,7 @@ export default function Team() {
     <div class="property-description">
       <h5>Spandan Sarkar </h5>
       <p>Event Head</p>
-      <a href="https://www.linkedin.com/in/bengal-e-summit-32394b315?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
+      <a href="https://www.linkedin.com/in/spandan-sarkar-65523322a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
     </div>
     <a href="/">
       
@@ -70,7 +70,7 @@ export default function Team() {
     <div class="property-description">
       <h5>Megha Biswas</h5>
       <p>Event Head</p>
-      <a href="https://www.linkedin.com/in/bengal-e-summit-32394b315?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
+      <a href="https://www.linkedin.com/in/megha-biswas-0079a8243?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
     </div>
     <a href="/">
     </a>
@@ -87,7 +87,7 @@ export default function Team() {
     <div class="property-description">
       <h5>Anushka Maji</h5>
       <p>PR Lead</p>
-      <a href="https://www.linkedin.com/in/bengal-e-summit-32394b315?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
+      <a href="https://www.linkedin.com/in/anushka-maji-1252b124a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
     </div>
     <a href="/">
      
@@ -105,7 +105,7 @@ export default function Team() {
     <div class="property-description">
       <h5>Ayush Bera</h5>
       <p>Collaborations Lead</p>
-      <a href="/"><img class="linklogo" src={linklogo} alt=""/></a>
+      <a href="https://www.linkedin.com/in/ayush-bera-b14b39227?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
     </div>
     <a href="/">
      
@@ -123,7 +123,7 @@ export default function Team() {
     <div class="property-description">
       <h5>Mayukh Mazumdar</h5>
       <p>Sponsorship Lead</p>
-      <a href="https://www.linkedin.com/in/bengal-e-summit-32394b315?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
+      <a href="https://www.linkedin.com/in/mayukh-majumder-83a19620b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
     </div>
     <a href="/">
     </a>
@@ -140,7 +140,7 @@ export default function Team() {
     <div class="property-description">
       <h5>Anuran De</h5>
       <p>Database and Logistics Lead</p>
-      <a href="https://www.linkedin.com/in/bengal-e-summit-32394b315?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
+      <a href="https://www.linkedin.com/in/anuran-de-7b7083286?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
     </div>
     <a href="/">
     </a>
@@ -157,7 +157,7 @@ export default function Team() {
     <div class="property-description">
       <h5>Saranya Chattopadhyay</h5>
       <p>Corporate Relations Lead</p>
-      <a href="https://www.linkedin.com/in/bengal-e-summit-32394b315?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
+      <a href="https://www.linkedin.com/in/saranya-chattopadhyay-06a153230?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
     </div>
     <a href="/">
     </a>
@@ -174,7 +174,7 @@ export default function Team() {
     <div class="property-description">
       <h5>Sayantika Neogi</h5>
       <p>Guest Relations</p>
-      <a href="https://www.linkedin.com/in/bengal-e-summit-32394b315?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
+      <a href="https://www.linkedin.com/in/sayantika-neogi-8b5a18274?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
     </div>
     <a href="/">
     </a>
@@ -191,7 +191,7 @@ export default function Team() {
     <div class="property-description">
       <h5>Prerona Paul</h5>
       <p>PR Lead</p>
-      <a href="https://www.linkedin.com/in/bengal-e-summit-32394b315?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
+      <a href="https://www.linkedin.com/in/prerona-paul-96bb90289?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
     </div>
     <a href="/">
     </a>
@@ -208,7 +208,7 @@ export default function Team() {
     <div class="property-description">
       <h5>Rajnandini Ghosh</h5>
       <p>Corporate Relations</p>
-      <a href="https://www.linkedin.com/in/bengal-e-summit-32394b315?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img class="linklogo" src={linklogo} alt=""/></a>
+      <a href=""><img class="linklogo" src={linklogo} alt=""/></a>
     </div>
     <a href="/">
     </a>
